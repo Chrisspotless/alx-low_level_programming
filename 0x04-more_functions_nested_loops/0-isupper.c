531a36666503615d4672052052ff9a4997fb42ab
Chrisspotless <chrisenoghase@gmail.com>
2023-02-23 20:07:28 -0800

    task0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,12 +6,12 @@
  *
  * Return: 0 or 1
  */
-int _isupper(int x)
-{
-	if (x >= 65 && x <= 90)
-	{
-		return (1);
-	}
+int _isupper(int c)
 
-	return (0);
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+
 }
